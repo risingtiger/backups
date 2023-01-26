75e1956392f16bccfc7d79952152a6bead014065
Davis Hammon <accounts@risingtiger.com>
2023-01-26 10:56:43 -0700

    ball of wax

--- xenitionbackup.sh
+++ xenitionbackup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-rsync -avihr --delete --exclude-from=/Users/xenition/Code/backup/excludes.txt ~/ /Volumes/CellarDisk/Backup\ Xenition/
+rsync -avihr --delete --no-links --exclude-from=/Users/dave/Code/backups/excludes.txt ~/ /Users/dave/Cloud/z__HomeDirectoryBackup/
+rsync -avihr --delete ~/.config ~/.ssh ~/.zshrc  /Users/dave/Cloud/z__HomeDirectoryBackup/
